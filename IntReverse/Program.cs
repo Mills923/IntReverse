829282bd015c8cc44054a0a1259a0b0cc5009ea8
David <david.mills2@outlook.com>
2021-05-04 10:48:58 -0700

    Add project files.

---
+++ IntReverse/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace IntReverse
+{
+    class Program
+    {
+        public static void Number(int input)
+        {
+            string holder = input.ToString();
+            string result = "";
+            int counter = 0;
+            for (int i = holder.Length - 1; i >= 0; i--)
+            {
+                if (holder[i] == '-')
+                {
+                    goto Skip;
+                }
+                result += holder[i];
+            }
+            Skip:
+
+            Console.WriteLine($"The number you passed in was: {input} \n Reversed is: {result}");
+        }
+
+        static void Main(string[] args)
+        {
+            Number(-100);
+            Number(1254634534);
+        }
+    }
+}
